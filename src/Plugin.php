7d8ac3ac720077868e366bc458ec54b160bbff9f
Andreas Sundqvist <andreas@stefna.is>
2020-10-27 13:48:55 +0100

    Add support for composer 2

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,12 +5,16 @@ namespace Circli\Installer;
 use Circli\Installer\Installers\ExtensionInstaller;
 use Circli\Installer\Installers\ModuleInstaller;
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\InstallerInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\EventDispatcher\EventSubscriberInterface;
 
 class Plugin implements PluginInterface
 {
+    /** @var InstallerInterface[] */
+    private $installers = [];
+
     /**
      * Apply plugin modifications to Composer
      *
@@ -21,25 +25,37 @@ class Plugin implements PluginInterface
     {
         $extra = $composer->getPackage()->getExtra();
 
-        $circliInstallers = [
+        $this->installers = [
             new ModuleInstaller($io, $composer),
             new ExtensionInstaller($io, $composer),
         ];
         if (isset($extra['moya']['plugins'])) {
             foreach ($extra['moya']['plugins'] as $installerClass) {
                 $installer = new $installerClass($composer);
-                $circliInstallers[] = $installer;
+                $this->installers[] = $installer;
             }
         }
 
         $installationManager = $composer->getInstallationManager();
         $eventDispatcher = $composer->getEventDispatcher();
 
-        foreach ($circliInstallers as $installer) {
+        foreach ($this->installers as $installer) {
             $installationManager->addInstaller($installer);
             if ($installer instanceof EventSubscriberInterface) {
                 $eventDispatcher->addSubscriber($installer);
             }
         }
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        $installationManager = $composer->getInstallationManager();
+        foreach ($this->installers as $installer) {
+            $installationManager->removeInstaller($installer);
+        }
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
     }
 }
